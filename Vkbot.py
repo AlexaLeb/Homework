9b71a70f3f335127ef9c1ea69a5cccf80d8faca4
AlexaLeb <118658187+AlexaLeb@users.noreply.github.com>
2023-02-12 20:00:26 +0300

    sasa

---
+++ Vkbot.py
@@ -0,0 +1,133 @@
+from random import randrange
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+import datetime
+import requests
+
+with open('tookenofbot.txt', 'r') as file:
+    bot_token = file.readline()
+with open('usertooken.txt', 'r') as file:
+    user_token = file.readline()
+vk = vk_api.VkApi(token=bot_token)
+vk2 = vk_api.VkApi(token=user_token)
+longpoll = VkLongPoll(vk)
+
+
+class VKbot:
+    def __init__(self, user_id: str):
+        self.user_id = user_id
+        self.user_info = self.get_user_id()
+        self.parts = self.find_part()
+
+    def write_msg(self, message):
+        vk.method('messages.send', {'user_id': self.user_id, 'message': message, 'random_id': randrange(10 ** 7), })
+
+    def age(self, date: str) -> int:
+        age = datetime.datetime.now().year - int(date[-4:])
+        return age
+
+    def get_user_id(self) -> dict:
+        user_info = {}
+        response = vk.method('users.get', {'user_id': self.user_id,
+                                           'v': 5.131,
+                                           'fields': 'first_name, last_name, bdate, sex, city'})
+        if response:
+            for key, value in response[0].items():
+                if key == 'city':
+                    user_info[key] = value['id']
+                elif key == 'bdate':
+                    user_info['age'] = self.age(value)
+                    user_info['bdate'] = value
+                else:
+                    user_info[key] = value
+        else:
+            self.write_msg('Ошибка')
+        return user_info
+
+    def check_missing_info(self, user_info: dict):
+        # Записывает в словрь отсутствующую информацию
+        info_missing = []
+        for item in ['bdate', 'sex', 'city']:
+            if not user_info.get(item):
+                info_missing.append(item)
+            if user_info.get('bdate'):
+                if len(user_info['bdate'].split('.')) != 3:
+                    info_missing.append('bdate')
+        return info_missing
+
+    def det_addinfo(self, field):
+        di = {
+            'bdate': 'вашу дату рождения в формате dd.mm.yyyy',
+            'city': 'ваш город'
+        }
+        self.write_msg(f'''Недостаточно  о вас информации, введите следующие данные: \n{di[field]}''')
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW:
+                if event.to_me:
+                    if field == 'city':
+                        return self.get_city(event.text)
+                    elif field == 'bdate':
+                        if len(event.text.split('.')) != 3:
+                            self.write_msg('неверно указана дата рождения')
+                        return event.text
+
+    def get_city(self, city):
+        di = {
+            'country_id': 1,
+            'q': city,
+            'count': 1
+        }
+
+        response = vk2.method('database.getCities', values=di)
+        if response['items']:
+            city_id = response['items'][0]['id']
+            return city_id
+        else:
+            self.write_msg('неверно указан город')
+            return False
+
+    def find_part(self,):
+        response = vk2.method('users.search', {
+            'age_from': self.user_info['age'] - 3,
+            'age_to': self.user_info['age'] + 3,
+            'sex': 3 - self.user_info['sex'],
+            'city': self.user_info['city'],
+            'statis': 1 or 6,
+            'has_photo': 1,
+            'count': 1000,
+            'sort': 0,
+            'v': 5.131
+        })
+
+        if response:
+            if response.get('items'):
+                return response.get('items')
+            else:
+                self.write_msg('ошибка')
+                return False
+
+    def get_person(self, number=1) -> str:
+        name = self.parts[number]['first_name']
+        surname = self.parts[number]['last_name']
+        url = 'vk.com/id' + str(self.parts[number]['id'])
+        return f'{name} {surname}, ссылка на странницу: {url}'
+
+    def photo_get(self, number):
+        response = vk2.method('photos.get', {
+            'owner_id': self.parts[number]['id'],
+            'count': 25,
+            'album_id': 'profile',
+            'rev': 1
+        })
+        c = 0
+        listofphoto = []
+        for info in list(response.values())[1]:
+                if c > 2:
+                    break
+                photo = requests.get(info['sizes'][-1]['url'])
+                print(info['sizes'][-1]['url'])
+                c += 1
+                listofphoto.append(photo)
+                vk.method('messages.send', {'user_id': self.user_id, 'attachment': photo+self.parts[number]['id']+, 'random_id': randrange(10 ** 7),'message': 'photo' })
+
+
--- main.py
+++ main.py
@@ -1,105 +1,27 @@
-from random import randrange
+from vk_api.longpoll import VkEventType
+import json
+import Vkbot
+from Vkbot import VKbot, longpoll
 
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+def get_button(text, color):
+    return {
+        "action": {
+            "type": "text",
+            "payload": "{\"button\": \"" + "1" + "\"}",
+            "label": f"{text}"
+        },
+        "color": f"{color}"
+    }
 
-with open('tookenofbot.txt', 'r') as file:
-    bot_token = file.readline()
-with open('usertooken.txt', 'r') as file:
-    user_token = file.readline()
-
-vk = vk_api.VkApi(token=bot_token)
-vk2 = vk_api.VkApi(token=user_token)
-longpoll = VkLongPoll(vk)
-
-class Vkbot:
-    def __int__(self, user_id: str):
-        self.user_id = user_id
-    def write_msg(message):
-        vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7), })
-
-
-    def get_user_id(user_id):  # создает словарь с информацией о поользователе
-        user_info = {}
-        response = vk.method('users.get', {'user_id': user_id,
-                                          'v': 5.131,
-                                          'fields': 'first_name, last_name, bdate, sex, city'})
-        if response:
-            for key, value in response[0].items():
-                if key == 'city':
-                    user_info[key] = value['id']
-                else:
-                    user_info[key] = value
-        else:
-            write_msg(user_id, 'Ошибка')
-            return False
-        return user_info
-
-
-    def check_missing_info(user_info):
-        # Записывает в словрь отсутствующую информацию
-        info_missing = []
-        for item in ['bdate', 'sex', 'city']:
-            if not user_info.get(item):
-                info_missing.append(item)
-            if user_info.get('bdate'):
-                if len(user_info['bdate'].split('.')) != 3:
-                    info_missing.append('bdate')
-        return info_missing
-
-
-    def det_addinfo(user_id, field):
-        di = {
-            'bdate': 'вашу дату рождения в формате dd.mm.yyyy',
-            'city': 'ваш город'
-        }
-        write_msg(user_id, f'''Недостаточно  о вас информации, введите следующие данные: \n{di[field]}''')
-        for event in longpoll.listen():
-            if event.type == VkEventType.MESSAGE_NEW:
-                if event.to_me:
-                    if field == 'city':
-                        return get_city(user_id, event.text)
-                    elif field == 'bdate':
-                        if len(event.text.split('.')) != 3:
-                            write_msg(user_id, 'неверно указана дата рождения')
-                        return event.text
-
-
-    def get_city(user_id, city):
-        di = {
-            'country_id': 1,
-            'q': city,
-            'count': 1
-        }
-
-        response = vk2.method('database.getCities', values=di)
-        if response['items']:
-            city_id = response['items'][0]['id']
-            return city_id
-        else:
-            write_msg(user_id, 'неверно указан город')
-            return False
-
-
-    def find_part(user_info):
-        response = vk2.method('users.search', {
-            'age_from': user_info['age'] - 3,
-            'age_to': user_info['age'] + 3,
-            'sex': 3 - user_info['sex'],
-            'city': user_info['city'],
-            'statis': 6,
-            'has_photo': 1,
-            'count': 1000,
-            'sort': 0,
-            'v': 5.131
-        })
-
-        if response:
-            if response.get('items'):
-                return response.get('items')
-            else:
-                write_msg(user_info['id'], 'ошибка')
-                return False
+keyboard = {
+    "one_time": False,
+    "buttons": [
+        [get_button('Начать поиск', 'primary')],
+        [get_button('Вперёд', 'secondary')]
+    ]
+}
+keyboard = json.dumps(keyboard, ensure_ascii=False).encode('utf-8')
+keyboard = str(keyboard.decode('utf-8'))
 
 
 
@@ -108,14 +30,24 @@ if __name__ == '__main__':
         if event.type == VkEventType.MESSAGE_NEW:
 
             if event.to_me:
-                request = event.text
+                request = event.text.lower()
+                ids = event.user_id
+                user = VKbot(ids)
 
-                if request == "привет" or request == "Привет":
-                    write_msg(event.user_id, f"Хай, {event.user_id}")
-                    write_msg(event.user_id, get_user_id('408198842'))
-                    print(get_user_id('408198842'))
+                if request == "начать поиск":
+                    user.write_msg(f"Ищу, {user.user_info['first_name']}")
+                    l = len(user.parts)
+                    c = 0
+                    while c <= 5:
+                        print(user.get_person(c))
+                        print(user.photo_get(c))
+                        user.write_msg(user.get_person(c)[0:])
+                        c += 1
+
+
+
+
                 elif request == "пока":
-                    write_msg(event.user_id, "Пока((")
+                    user.write_msg("Пока((")
                 else:
-                    write_msg(event.user_id, "Не понял вашего ответа...")
-
+                    user.write_msg("Не понял вашего ответа...")
